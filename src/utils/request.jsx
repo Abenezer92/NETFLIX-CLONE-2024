a206b0fb14c1c0b7b15eed5a1be1117f7345928b
abenezerdejene <abenezerdejene8@gmail.com>
2024-10-29 13:06:18 +0400

    for deployement service

---
+++ src/utils/request.jsx
@@ -0,0 +1,16 @@
+// const API_KEY = process.env.REACT_APP_API_KEY;app js
+const API_KEY = import.meta.env.VITE_API_KEY; // FOR VITE
+
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with-networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with-genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with-genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with-genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with-genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with-genres=99`,
+  fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+};
+
+export default request;
